632ef82ee4283e0024181e04693d6c1c6ab145b0
danydownload <danieletortoli@gmail.com>
2023-07-18 16:29:40 +0200

    Refactor project structure and file names
    Moved Server.py and Client.py to the 'src' folder and reorganized the project structure. Now the 'src' folder contains the following files:
    
    - enums.py: Contains enumerations for moves, results, and match status.
    - game.py: Defines the Game class responsible for managing game logic.
    - gameclient.py: Implements the GameClient class for the game client functionality.
    - gamegui.py: Implements the GameGUI class for the game graphical user interface.
    - gameserver.py: Implements the GameServer class for managing game sessions.
    
    Reasons for the changes:
    
    1. Improved project organization: By creating a separate 'src' folder, it provides a cleaner and more organized project structure. Each file now has its own dedicated module for specific functionality.
    
    2. Modularization: Splitting the code into separate files promotes modularity and reusability. Each module now focuses on a specific aspect of the game, making it easier to understand, maintain, and extend the codebase.
    
    3. Code readability: With distinct file names and organized structure, it becomes easier to locate and navigate through different components of the application.
    
    These changes aim to enhance the overall maintainability and scalability of the project, making it easier to add new features or make modifications in the future.

---
+++ src/enums.py
@@ -0,0 +1,24 @@
+# enums.py
+
+from enum import Enum
+
+
+# Enum per le mosse
+class Move(Enum):
+    ROCK = "rock"
+    PAPER = "paper"
+    SCISSORS = "scissors"
+
+
+# Enum per i risultati
+class Result(Enum):
+    WIN = "Winner"
+    LOSE = "Loser"
+    DRAW = "Draw"
+
+
+# Enum per lo stato della partita
+class MatchStatus(Enum):
+    REMATCH = "REMATCH"
+    OVER = "OVER"
+    ONGOING = "ONGOING"
